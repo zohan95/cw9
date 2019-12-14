bfbbb5ea3ccc78c9f2b041c6723d0b1b4c87994d
Ваше Имя <you@example.com>
2019-12-14 12:33:01 +0600

    Написаны модели;

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(verbose_name='Фотография', upload_to='uploads')
+    sign = models.CharField(max_length=63, verbose_name='Подпись')
+    date_create = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0, verbose_name='Лайки')
+    author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.PROTECT, related_name='photo_user')
+    liked = models.ManyToManyField(User, related_name='user_liked', blank=True)
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=255, verbose_name='Комментарий')
+    photo = models.ForeignKey(Photo, verbose_name='Фотография', on_delete=models.CASCADE, related_name='comment_photo')
+    author = models.ForeignKey(User, verbose_name='Автор', related_name='comment_author', on_delete=models.PROTECT)
+    date_create = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
